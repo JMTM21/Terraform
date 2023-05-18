c4b069f05906976bd3be851a0a8edbec297e476a
Jack Meehan <jack.meehan@primarygoal.ac.uk>
2023-05-18 12:14:15 +0100

    template done

--- main.tf
+++ main.tf
@@ -3,7 +3,45 @@ resource "azurerm_resource_group" "az-rsg" {
 
     name = "rsg-demo"
     location = "UK South"
-    tags = "rsg-demo"
+    tags = {
+
+    }
   
+}
+
+resource "azurerm_network_security_group" "az-nsg" {
+
+    name = "nsg-demo"
+    location = azurerm_resource_group.az-rsg.location
+    resource_group_name = azurerm_resource_group.az-rsg.name
+
+    security_rule {
+    }
+
+    security_rule {
+    }
+
+
+  
+}
+
+resource "azurerm_virtual_network" "az-vnet" {
+     
+     name = "vnet-demo"
+     location = azurerm_resource_group.az-rsg.location
+     resource_group_name = azurerm_resource_group.az-rsg.name
+     address_space  = ["10.0.0.0/16"]
+     subnet  {
+
+        
+     }
+
+
+  
+}
+
+resource "azurerm_network_interface" "az-nic" {
+  
+
 }
 
---
+++ vm.tf
@@ -0,0 +1,4 @@
+resource "azurerm_virtual_machine" "az-vm" {
+    
+  
+}
