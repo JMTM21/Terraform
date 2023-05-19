9d9ff8e9410cb7b1a30b3470ce8ed1cfd5612789
Jack Meehan <jack.meehan@primarygoal.ac.uk>
2023-05-19 12:06:28 +0100

    auto account

--- terraform.tfvars
+++ terraform.tfvars
@@ -2,3 +2,4 @@ location       = "UK South"
 admin_password = "value"
 admin_username = "value"
 tag            = "demo"
+
--- vm.tf
+++ vm.tf
@@ -24,9 +24,10 @@ resource "azurerm_virtual_machine" "az-vm" {
   resource_group_name   = azurerm_resource_group.az-rsg.name
   location              = azurerm_resource_group.az-rsg.location
   network_interface_ids = [azurerm_network_interface.az-nic.id]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "Standard_F2"
+  admin_username        = var.admin_username
 
-#specifies image 
+  #specifies image 
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
@@ -35,29 +36,15 @@ resource "azurerm_virtual_machine" "az-vm" {
 
   }
 
-#config disk 
-  storage_os_disk {
-    name              = "osdisk-demo"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+  #config disk 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
 
   }
 
   #config profile
 
-  os_profile {
-    computer_name  = "vmserver-demo"
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-
-  }
-
-  os_profile_linux_config {
-
-    disable_password_authentication = false
-
-  }
 
 
   tags = {
