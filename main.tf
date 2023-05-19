753576fafb885641afd5ce91e414a893ba536db4
jackmeehan <jackmeehan1@gmail.com>
2023-05-19 20:28:48 +0100

    nic

--- main.tf
+++ main.tf
@@ -57,18 +57,15 @@ resource "azurerm_virtual_network" "az-vnet" {
   resource_group_name = azurerm_resource_group.az-rsg.name
   address_space       = ["10.0.0.0/16"]
   #creates subnet
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-    security_group = azurerm_network_security_group.az-nsg.id
-
-  }
   tags = {
     enviroment = var.tag
   }
 
-
-
 }
-
+resource "azurerm_subnet" "az-sub" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.az-rsg.name
+  virtual_network_name = azurerm_virtual_network.az-vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
